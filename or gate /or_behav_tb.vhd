b9651ae645d38321993961ad1c6ed5bee819080b
JOYAL BABU <joyalbabujp21@gmail.com>
2023-05-17 17:07:47 +0530

    Update or_behav_tb.vhd

--- or gate /or_behav_tb.vhd
+++ or gate /or_behav_tb.vhd
@@ -7,18 +7,31 @@ end or_behav_tb;
 
 Architecture Behavioral of or_behav_tb is
 
- signal  A :  STD_LOGIC;
- signal  B :  STD_LOGIC;
+component or_behav is
+   Port (
+         A : in  STD_LOGIC;
+         B : in  STD_LOGIC;
+         C : out STD_LOGIC);
+end component;
+
+signal  A :  STD_LOGIC := '0';
+signal  B :  STD_LOGIC := '0';
+signal  C :  STD_LOGIC := '0';
 
 Begin
+--instantiate design under test
+DUT : or_behav port map(
+        A => A,
+        B => B,
+        C => C);
 
-    stimulus_process : process
-        Begin
-            A <= '0';  B <= '0'; wait for 500 ns;
-            A <= '0';  B <= '1'; wait for 500 ns;
-            A <= '1';  B <= '0'; wait for 500 ns;
-            A <= '1';  B <= '1'; wait for 500 ns;
-            wait;
-    end process;
+stimulus_process : process
+    Begin
+        A <= '0';  B <= '0'; wait for 500 ns;
+        A <= '0';  B <= '1'; wait for 500 ns;
+        A <= '1';  B <= '0'; wait for 500 ns;
+        A <= '1';  B <= '1'; wait for 500 ns;
+        wait;
+end process;
 
 end Behavioral;
